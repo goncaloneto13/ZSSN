d514507057aa04920a7403595f29b1dc0bebc0a8
goncaloneto13 <g.neto13hotmail.com>
2022-04-17 19:37:04 -0300

    placeholder data de nascimento

--- ZSSN_app/forms.py
+++ ZSSN_app/forms.py
@@ -5,9 +5,14 @@ from django import forms
 from ZSSN_app.models import Inventario, Sobrevivente
 
 class SobreviventeForm(forms.ModelForm):
+
     class Meta:
         model = Sobrevivente
         exclude = ['infectado','acusacoes','infectados_relatados']
+        widgets = { 'data_n': forms.DateInput(format=('%d-%m-%Y'), 
+                                             attrs={'class':'data_n', 
+                                            'placeholder':'dd/mm/aaaa'})
+                    }
 
 class LocalForm(forms.ModelForm):
     class Meta:
